3b2e6baf81013fcd335fac72699602dd012d5b33
Michael <suarezmichael@estudiantec.cr>
2022-08-28 21:10:04 -0600

    se agregaron botones para retroceder en las interfaces

--- CE Music Player/src/main/java/com/example/cemusicplayer/UserController.java
+++ CE Music Player/src/main/java/com/example/cemusicplayer/UserController.java
@@ -2,12 +2,21 @@ package com.example.cemusicplayer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserController {
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private Button UserLoader;
 
@@ -32,6 +41,14 @@ public class UserController {
         System.out.println(Username);
         System.out.println(Password);
         System.out.println(Email);
+    }
+    public void Login(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
 }
