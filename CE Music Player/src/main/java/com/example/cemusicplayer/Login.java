2ef16b32b7b21cf9bd90f670625536235f34517e
Michael <suarezmichael@estudiantec.cr>
2022-08-28 20:19:08 -0600

    Se crearon las demas interfaces para el registro de usuario

---
+++ CE Music Player/src/main/java/com/example/cemusicplayer/Login.java
@@ -0,0 +1,35 @@
+package com.example.cemusicplayer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void SignUp(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void SignIn(ActionEvent event)throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
